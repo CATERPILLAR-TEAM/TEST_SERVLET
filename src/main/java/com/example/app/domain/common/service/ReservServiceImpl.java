ea583a462ebd74457b479aff92251d2d384bb761
JIWON55 <qhdk3071@naver.com>
2024-04-26 15:44:52 +0900

    .

--- src/main/java/com/example/app/domain/common/service/ReservServiceImpl.java
+++ src/main/java/com/example/app/domain/common/service/ReservServiceImpl.java
@@ -1,52 +1,45 @@
 package com.example.app.domain.common.service;
 
-import java.util.List;
-
 import com.example.app.domain.common.dao.ConnectionPool;
 import com.example.app.domain.common.dao.ReservDao;
 import com.example.app.domain.common.dao.ReservDaoImpl;
 import com.example.app.domain.common.dto.ReservDto;
 
-public class ReservServiceImpl implements ReservService{
-	
+public class ReservServiceImpl implements ReservService {
+
 	private ReservDao dao;
 	private ConnectionPool connectionPool;
-	
+
 	private static ReservService instance;
+
 	public static ReservService getInstance() throws Exception {
-		if(instance==null)
-			instance=new ReservServiceImpl();
+		if (instance == null)
+			instance = new ReservServiceImpl();
 		return instance;
 	}
-	
+
 	private ReservServiceImpl() throws Exception {
 		dao = ReservDaoImpl.getInstance();
 		this.connectionPool = ConnectionPool.getInstance();
 	}
-	
-	
+
 	@Override
 	public boolean reservRegister(ReservDto dto) throws Exception {
-		//TX
-			connectionPool.txStart();			//05-01 Day TX
-			boolean result = dao.Insert(dto);	//05-01 Day TX
-			connectionPool.txCommit();			//05-01 Day TX
-				
-				return result;
-	}
+		// TX
+		connectionPool.txStart(); // 05-01 Day TX
+		System.out.println(dto + "3 - service");
+		boolean result = dao.Insert(dto); // 05-01 Day TX
+		connectionPool.txCommit(); // 05-01 Day TX
 
-	
+		return result;
+	}
 
 	@Override
 	public ReservDto getReservation(int userId) throws Exception {
-		
+
 		ReservDto dto = dao.Select(userId);
-		
+
 		return dto;
 	}
 
-	
-
-	
-	
 }
